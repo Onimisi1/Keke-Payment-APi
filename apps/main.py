8c0524d5626168ca16aa6d7ff26d6b64c3d220ff
Onimisi <onimisiazeez7@gmail.com>
2024-08-27 13:59:07 +0100

    Initial Commit

---
+++ apps/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import models
+from database import engine
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+
+@app.get("/")
+async def home():
+    return {"Hello World"}
